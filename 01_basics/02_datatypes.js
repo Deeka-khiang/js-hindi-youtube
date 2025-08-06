e98730569facd1fc0a5137a83815a81813b10b8f
Deeka-khiang <deekachakma8@gmail.com>
2025-08-06 05:36:48 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodeJS not browser
+
+// readability
+ let name = "Deeki"
+ let age = 19
+ let isLoggingIn = false
+
+
+ // number => 2 to power 53
+ // bigInt
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined => not assigned value
+ // symbol => to find uniqueness
+
+
+
+ // object
+
+
+
+ console.log(typeof name);
+ 
